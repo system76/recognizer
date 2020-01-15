c55a23b789b056bbb38e7b7988ce57bd2c2f108b
Blake Kostner <git@btkostner.io>
2020-01-15 13:58:10 -0700

    update style in releases config

--- config/releases.exs
+++ config/releases.exs
@@ -2,7 +2,13 @@ import Config
 
 config :recognizer, RecognizerWeb.Endpoint,
   url: [host: System.get_env("HOST"), port: 443],
-  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "80")],
+  http: [
+    :inet6,
+    port:
+      System.get_env()
+      |> Map.get("PORT", "4000")
+      |> String.to_integer()
+  ],
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   force_ssl: [hstl: true]
 
@@ -11,6 +17,9 @@ config :recognizer, Recognizer.Repo,
   password: System.get_env("DB_PASSWORD"),
   database: System.get_env("DB_NAME"),
   hostname: System.get_env("DB_HOST"),
-  pool_size: String.to_integer(System.get_env("DB_POOL") || "10")
+  pool_size:
+    System.get_env()
+    |> Map.get("DB_POOL", "10")
+    |> String.to_integer()
 
 config :logger, level: :warn
