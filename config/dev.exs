65893558ab9b185395ddaf8bd53f7184767321cc
Mike Garegnani <systems.support@gmail.com>
2020-12-04 17:04:32 -0700

    Adding webpack to watcher

--- config/dev.exs
+++ config/dev.exs
@@ -20,7 +20,13 @@ config :recognizer, RecognizerWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: []
+  watchers: [node: [
+      "node_modules/webpack/bin/webpack.js",
+      "--mode",
+      "development",
+      "--watch-stdin",
+      cd: Path.expand("../assets", __DIR__)
+    ]]
 
 # ## SSL Support
 #
