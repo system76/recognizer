9b5e1e2f65aff9b457f212a4696cf77b6c99d6a7
Blake Kostner <git@btkostner.io>
2020-12-08 22:39:20 -0700

    update http to port 8080 and remove ipv6 socket stuff

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ import Config
 
 config :recognizer, RecognizerWeb.Endpoint,
   url: [port: 443],
-  http: [port: 4000, transport_options: [socket_opts: [:inet6]]],
+  http: [port: 8080],
   cache_static_manifest: "priv/static/cache_manifest.json",
   gzip: true,
   server: true
