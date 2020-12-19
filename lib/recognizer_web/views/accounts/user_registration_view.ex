ec940caa57d2c3c64122d63b96d80ffa2b472f10
Blake Kostner <git@btkostner.io>
2020-12-18 21:17:08 -0700

    add extra user profile fields

--- lib/recognizer_web/views/accounts/user_registration_view.ex
+++ lib/recognizer_web/views/accounts/user_registration_view.ex
@@ -1,3 +1,10 @@
 defmodule RecognizerWeb.Accounts.UserRegistrationView do
   use RecognizerWeb, :view
+
+  def business_type_class(changeset) do
+    case Ecto.Changeset.get_field(changeset, :type) do
+      :individual -> "none"
+      :business -> "block"
+    end
+  end
 end
