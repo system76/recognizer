2d5bbf2e3e98c6bcb4ec44373ce571d529e9afa0
Blake Kostner <git@btkostner.io>
2020-12-21 12:58:54 -0700

    fix responses to work with oauth two factor

--- lib/recognizer_web/controllers/accounts/user_oauth_controller.ex
+++ lib/recognizer_web/controllers/accounts/user_oauth_controller.ex
@@ -65,7 +65,7 @@ defmodule RecognizerWeb.Accounts.UserOAuthController do
     Repo.transaction(fn ->
       with {:ok, user} <- Accounts.register_oauth_user(user_params),
            {:ok, _oauth} <- Accounts.create_oauth(user, provider, uid) do
-        user
+        {:ok, user}
       else
         {:error, changeset} ->
           Repo.rollback(changeset)
