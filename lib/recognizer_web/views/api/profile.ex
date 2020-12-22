e637e8e3b7d9fbe39818979da62da98865565ded
Blake Kostner <git@btkostner.io>
2020-12-21 17:42:00 -0700

    format

--- lib/recognizer_web/views/api/profile.ex
+++ lib/recognizer_web/views/api/profile.ex
@@ -6,18 +6,13 @@ defmodule RecognizerWeb.Api.ProfileView do
   def render("show.json", %{user: user}) do
     %{
       id: user.id,
-
       first_name: user.first_name,
       last_name: user.last_name,
-
       email: user.email,
       phone_number: user.phone_number,
-
       type: user.type,
       company_name: user.company_name,
-
       newsletter: user.newsletter,
-
       admin: Role.admin?(user),
       notification_preferences: render("notification_preferences.json", user)
     }
