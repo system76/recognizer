2c65c04487c9897d137cb0e89132f19b703e4743
Sean Callan <doomspork@users.noreply.github.com>
2021-01-07 11:53:34 -0700

    All logout via GET (#58)

--- test/recognizer_web/controllers/accounts/user_session_controller_test.exs
+++ test/recognizer_web/controllers/accounts/user_session_controller_test.exs
@@ -77,15 +77,13 @@ defmodule RecognizerWeb.Accounts.UserSessionControllerTest do
 
   describe "DELETE /users/log_out" do
     test "logs the user out", %{conn: conn, user: user} do
-      conn = conn |> log_in_user(user) |> delete(Routes.user_session_path(conn, :delete))
+      conn = conn |> log_in_user(user) |> get(Routes.user_session_path(conn, :delete))
       assert redirected_to(conn) == "/"
-      assert get_flash(conn, :info) =~ "Logged out successfully"
     end
 
     test "succeeds even if the user is not logged in", %{conn: conn} do
-      conn = delete(conn, Routes.user_session_path(conn, :delete))
+      conn = get(conn, Routes.user_session_path(conn, :delete))
       assert redirected_to(conn) == "/"
-      assert get_flash(conn, :info) =~ "Logged out successfully"
     end
   end
 end
