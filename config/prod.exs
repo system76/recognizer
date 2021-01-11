a45106d55da209b3666a6b708638f80b1ff65d28
Blake Kostner <btkostner@gmail.com>
2021-01-11 12:00:40 -0700

    store only the user id in session data (#66)

--- config/prod.exs
+++ config/prod.exs
@@ -2,12 +2,7 @@ import Config
 
 config :recognizer, RecognizerWeb.Endpoint,
   url: [scheme: "https", port: 443],
-  http: [
-    port: 8080,
-    protocol_options: [
-      max_header_value_length: 8192
-    ]
-  ],
+  http: [port: 8080],
   cache_static_manifest: "priv/static/cache_manifest.json",
   gzip: true,
   server: true
