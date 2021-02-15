5d48f635b66c6738a6444d252b4744c031be6557
Blake Kostner <git@btkostner.io>
2021-02-15 12:54:13 -0700

    add user_id to logger metadata

--- lib/recognizer_web/plugs/auth_plug.ex
+++ lib/recognizer_web/plugs/auth_plug.ex
@@ -3,6 +3,8 @@ defmodule RecognizerWeb.AuthPlug do
   `Guardian.Plug.Pipeline` for verifying JWT tokens in the session and header.
   """
 
+  require Logger
+
   use Guardian.Plug.Pipeline,
     otp_app: :recognizer,
     module: Recognizer.Guardian,
@@ -11,4 +13,14 @@ defmodule RecognizerWeb.AuthPlug do
   plug Guardian.Plug.VerifySession, claims: %{"iss" => "system76", "typ" => "access"}
   plug Guardian.Plug.VerifyHeader, claims: %{"iss" => "system76", "typ" => "access"}
   plug Guardian.Plug.LoadResource, allow_blank: true
+
+  plug :set_metadata
+
+  defp set_metadata(conn, _params) do
+    if user = Guardian.Plug.current_resource(conn) do
+      Logger.metadata(user_id: user.id)
+    end
+
+    conn
+  end
 end
