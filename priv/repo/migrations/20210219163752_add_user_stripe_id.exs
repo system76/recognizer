fe199afb007f96ec5111d4d6cc46697189e2da02
Blake Kostner <git@btkostner.io>
2021-02-19 09:40:09 -0700

    add comment about empty db migration

--- priv/repo/migrations/20210219163752_add_user_stripe_id.exs
+++ priv/repo/migrations/20210219163752_add_user_stripe_id.exs
@@ -1,6 +1,14 @@
 defmodule Recognizer.Repo.Migrations.AddUserStripeId do
   use Ecto.Migration
 
+  #  This migration is used for our existing database. It's commented out because
+  #  we will eventually move authentication to it's own database and this code
+  #  can be deleted.
+  #
+  #  def change do
+  #    #
+  #  end
+
   def change do
     alter table(:users) do
       add :stripe_id, :string
