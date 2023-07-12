f7779e4b8c2955e449a2be01518d1498b94a28fe
Mike Cifelli <26522946+macifell@users.noreply.github.com>
2023-07-12 14:36:22 -0400

    Implement name restrictions (#128)

--- test/recognizer_web/controllers/accounts/api/user_settings_controller_test.exs
+++ test/recognizer_web/controllers/accounts/api/user_settings_controller_test.exs
@@ -40,6 +40,16 @@ defmodule RecognizerWeb.Api.UserSettingsControllerTest do
       assert %{"user" => %{"id" => ^user_id, "first_name" => "Updated"}} = json_response(conn, 200)
     end
 
+    test "don't allow special characters in the first name", %{conn: conn} do
+      conn = put(conn, "/api/settings", %{"action" => "update", "user" => %{"first_name" => "http://example.com"}})
+      assert %{"errors" => %{"first_name" => ["must not contain special characters"]}} = json_response(conn, 400)
+    end
+
+    test "don't allow special characters in the last name", %{conn: conn} do
+      conn = put(conn, "/api/settings", %{"action" => "update", "user" => %{"last_name" => "http://example.com"}})
+      assert %{"errors" => %{"last_name" => ["must not contain special characters"]}} = json_response(conn, 400)
+    end
+
     test "PUT /api/settings with `update_password` action", %{conn: conn, user: user} do
       conn =
         put(conn, "/api/settings", %{
