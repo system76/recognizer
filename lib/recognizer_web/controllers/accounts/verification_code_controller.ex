d332c1b748c71df3d1ce2c2211f8cf7b9c2332bf
Mike Cifelli <26522946+macifell@users.noreply.github.com>
2023-07-13 18:37:52 -0400

    Send welcome email

--- lib/recognizer_web/controllers/accounts/verification_code_controller.ex
+++ lib/recognizer_web/controllers/accounts/verification_code_controller.ex
@@ -2,11 +2,13 @@ defmodule RecognizerWeb.Accounts.VerificationCodeController do
   use RecognizerWeb, :controller
 
   alias Recognizer.Accounts
+  alias Recognizer.Notifications.Account, as: Notification
   alias RecognizerWeb.Authentication
 
   def new(conn, %{"code" => code}) do
     case Accounts.verify_user(code) do
       {:ok, user} ->
+        Notification.deliver_user_created_message(user)
         Authentication.log_in_user(conn, user)
 
       {:error, error} ->
