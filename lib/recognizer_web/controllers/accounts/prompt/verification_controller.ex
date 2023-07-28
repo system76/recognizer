f05574912d6a9516aba70d2b99375407603d4561
Mike Cifelli <26522946+macifell@users.noreply.github.com>
2023-07-28 10:40:49 -0400

    Add tests

--- lib/recognizer_web/controllers/accounts/prompt/verification_controller.ex
+++ lib/recognizer_web/controllers/accounts/prompt/verification_controller.ex
@@ -1,9 +1,15 @@
 defmodule RecognizerWeb.Accounts.Prompt.VerificationController do
   use RecognizerWeb, :controller
 
+  alias RecognizerWeb.Authentication
+
   plug :ensure_user
 
-  def new(conn, _params) do
-    render(conn, "new.html", user: conn.assigns.user)
+  def new(%{assigns: %{user: %{verified_at: nil}}} = conn, _params) do
+    render(conn, "new.html")
+  end
+
+  def new(%{assigns: %{user: user}} = conn, _params) do
+    Authentication.log_in_user(conn, user)
   end
 end
---
+++ test/recognizer_web/controllers/accounts/prompt/verification_controller_test.exs
@@ -0,0 +1,40 @@
+defmodule RecognizerWeb.Accounts.Prompt.VerificationCodeControllerTest do
+  use RecognizerWeb.ConnCase
+
+  import Recognizer.AccountFactory
+
+  setup %{conn: conn} do
+    user = insert(:user, verified_at: nil)
+    verified_user = insert(:user)
+
+    %{
+      unverified_conn:
+        Phoenix.ConnTest.init_test_session(conn, %{
+          prompt_user_id: user.id
+        }),
+      verified_conn:
+        Phoenix.ConnTest.init_test_session(conn, %{
+          prompt_user_id: verified_user.id
+        }),
+      empty_conn: conn
+    }
+  end
+
+  describe "GET /prompt/verification" do
+    test "renders the verification required page for an unverified user", %{unverified_conn: conn} do
+      conn = get(conn, Routes.prompt_verification_path(conn, :new))
+      response = html_response(conn, 200)
+      assert response =~ "Account Verification Pending</h2>"
+    end
+
+    test "redirects verified user", %{verified_conn: conn} do
+      conn = get(conn, Routes.prompt_verification_path(conn, :new))
+      assert redirected_to(conn) =~ "/settings"
+    end
+
+    test "redirects anonymous user", %{empty_conn: conn} do
+      conn = get(conn, Routes.prompt_verification_path(conn, :new))
+      assert redirected_to(conn) =~ "/login"
+    end
+  end
+end
