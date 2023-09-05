7ab90301296e74cebff35d78e457fb320bf7a5d6
Mike Cifelli <26522946+macifell@users.noreply.github.com>
2023-09-05 18:14:59 -0400

    Verify users created internally

--- lib/recognizer/accounts.ex
+++ lib/recognizer/accounts.ex
@@ -184,6 +184,7 @@ defmodule Recognizer.Accounts do
     |> User.registration_changeset(attrs, opts)
     |> insert_user_and_notification_preferences()
     |> maybe_notify_new_user()
+    |> verify_user()
   end
 
   @doc """
