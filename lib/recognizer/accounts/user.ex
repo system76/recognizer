b59707ac00c479b50bc0532b19dcc3e003c737cc
Yongjin Chong <zephyranthes03@gmail.com>
2025-06-16 15:17:07 -0600

    Fix name filtering to match other mail services names (#165)
    * Fix name filtering to match other mail services names

--- lib/recognizer/accounts/user.ex
+++ lib/recognizer/accounts/user.ex
@@ -133,8 +133,12 @@ defmodule Recognizer.Accounts.User do
 
   defp validate_names(changeset) do
     changeset
-    |> validate_format(:first_name, ~r/^[\w\s\d\,\'\-\_\.]+$/u, message: "must not contain special characters")
-    |> validate_format(:last_name, ~r/^[\w\s\d\,\'\-\_\.]+$/u, message: "must not contain special characters")
+    |> validate_format(:first_name, ~r/^[A-Za-zÀ-ÖØ-öø-ÿ0-9'.\- ]{1,80}$/,
+      message: "Please enter a valid name using letters, spaces, and basic punctuation"
+    )
+    |> validate_format(:last_name, ~r/^[A-Za-zÀ-ÖØ-öø-ÿ0-9'.\- ]{1,80}$/,
+      message: "Please enter a valid name using letters, spaces, and basic punctuation"
+    )
     |> validate_length(:first_name, max: 80)
     |> validate_length(:last_name, max: 80)
   end
@@ -162,9 +166,7 @@ defmodule Recognizer.Accounts.User do
       |> validate_format(:password, ~r/[0-9]/, message: "must contain a number")
       |> validate_format(:password, ~r/[A-Z]/, message: "must contain an UPPERCASE letter")
       |> validate_format(:password, ~r/[a-z]/, message: "must contain a lowercase letter")
-      |> validate_format(:password, ~r/[ \!\$\*\+\[\{\]\}\\\|\.\/\?,!@#%^&-=,.<>'";:]/,
-        message: "must contain a symbol or space"
-      )
+      |> validate_format(:password, ~r/[ !$*+\[\]{}\\|.\/?@#%^&=<>'";:-]/, message: "must contain a symbol or space")
       |> validate_new_password(opts)
       |> maybe_hash_password(opts)
     end
